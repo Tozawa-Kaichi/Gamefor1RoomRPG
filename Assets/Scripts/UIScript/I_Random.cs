f32eaeb948f66abba0c343f9e0d9b11d00f27dec
st37071556@icloud.com <st37071556@icloud.com>
2022-07-04 20:56:11 +0900

    前に同じ IF

--- Assets/Scripts/UIScript/I_Random.cs
+++ Assets/Scripts/UIScript/I_Random.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// ランダムな要素からなにか動作するスクリプト用インターフェース
+/// </summary>
 public class I_Random : MonoBehaviour
 {
     interface RandomPool
