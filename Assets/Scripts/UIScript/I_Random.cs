b4d37ffa5840cda94164c4043fd69e043c48b72f
st37071556@icloud.com <st37071556@icloud.com>
2022-07-04 20:52:50 +0900

    インターフェースの利用

---
+++ Assets/Scripts/UIScript/I_Random.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class I_Random : MonoBehaviour
+{
+    interface RandomPool
+    {
+        void RandomPool();
+    }
+
+}
