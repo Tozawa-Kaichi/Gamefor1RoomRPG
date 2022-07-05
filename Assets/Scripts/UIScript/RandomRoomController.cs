d55dad8bc01c43fb826e4e73aa04531a11e74b4b
st37071556@icloud.com <st37071556@icloud.com>
2022-07-05 14:22:16 +0900

    ランダムに選ぶ際はif文で分岐させるよりListとCountを活用し、追加に際してスクリプトの変更が必要ないようにすた

--- Assets/Scripts/UIScript/RandomRoomController.cs
+++ Assets/Scripts/UIScript/RandomRoomController.cs
@@ -9,20 +9,23 @@ public class RandomRoomController : MonoBehaviour , I_Random
 {
     [Tooltip("ランダムに選ばれる部屋効果の一覧 ※0番目のインデックスはNonにしてください")]
     [SerializeField] List<Texture2D> _roomEffectIcons;
+    [Tooltip("ランダムに選ばれる部屋イベントの一覧 ※0番目のインデックスはNonにしてください")]
+    [SerializeField] List<Texture2D> _roomEventIcons;
 
     [SerializeField] int _percentOfZeroIndex = 10;
     public void RandomPool()
     {
         
     }
-    Texture2D RandomSelected()
+    Texture2D RandomEffectSelected()//ランダムな部屋効果の選出
     {
-        int random = Random.Range(0, 100);
-        if (random > _percentOfZeroIndex)
-        {
-            return _roomEffectIcons[1];
-        }
-        return _roomEffectIcons[0];
+        int random = Random.Range(0, _roomEffectIcons.Count);
+        return _roomEffectIcons[random];
+    }
+    Texture2D RandomEventSelected()//ランダムな部屋イベントの選出
+    {
+        int random = Random.Range(0, _roomEventIcons.Count);
+        return _roomEventIcons[random];
     }
 
     private void Start()
