53de8e1ad45f3264ec5e8195dc26460114db7290
st37071556@icloud.com <st37071556@icloud.com>
2022-07-12 23:14:41 +0900

    A*

--- Assets/Scripts/A_starScript/AstarController.cs
+++ Assets/Scripts/A_starScript/AstarController.cs
@@ -6,15 +6,5 @@ using UnityEngine;
 /// </summary>
 public class AstarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
